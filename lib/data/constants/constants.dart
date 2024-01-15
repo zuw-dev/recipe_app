ceb06f13a66a450af1d2253cdaff7c1f22f78a70
zuw-dev <35877586+zuw-dev@users.noreply.github.com>
2024-01-15 14:34:01 +0300

    Update constants.dart

--- lib/data/constants/constants.dart
+++ lib/data/constants/constants.dart
@@ -1,8 +1,8 @@
-const String apiKey = "27d12bef6b62496bb4c5d76c424567fd";
+const String apiKey = "";
 
 const String complexSearch =
     "https://api.spoonacular.com/recipes/complexSearch";
 const String recipeInfo = "https://api.spoonacular.com/recipes/";
 
 const String baseUrl =
-    "https://recipeapp-8d063-default-rtdb.europe-west1.firebasedatabase.app/";
+    "";
